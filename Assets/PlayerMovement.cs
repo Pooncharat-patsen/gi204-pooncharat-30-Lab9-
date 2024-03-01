cf8954d7636ff3bcb4348e58f271f9579dff3b90
Pooncharat-patsen <chikakukatozaz@gmail.com>
2024-03-01 10:45:48 +0700

    lab 1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f; 
+
+    // Update 
+    void Update()
+    {
+        // Vector3
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        
+        movement = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movement;
+
+       
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
